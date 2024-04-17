e62119aeec2d1160fea753b4c8de7366bd06f2af
melbyngarcon <143121166+melbyngarcon@users.noreply.github.com>
2024-04-17 19:49:21 +0000

    Finished

--- main.py
+++ main.py
@@ -10,33 +10,58 @@ def make_undirected_graph(edge_list):
 
 
 def reachable(graph, start_node):
-    """
-    Returns:
-      the set of nodes reachable from start_node
-    """
-    result = set([start_node])
-    frontier = set([start_node])
-    while len(frontier) != 0:
-        ###TODO
-        pass
-    return result
-
-
+  """
+  Returns:
+    the set of nodes reachable from start_node
+  """
+  result = set([start_node])
+  frontier = set([start_node])
+  while frontier:
+      current = frontier.pop()  # Remove one node from frontier
+      for neighbor in graph[current]:
+          if neighbor not in result:
+              result.add(neighbor)
+              frontier.add(neighbor)
+  return result
 
 
 
 def connected(graph):
-    ### TODO
-    pass
+    """
+    Determines if the graph is connected.
 
+    Returns:
+        True if the graph is connected, False otherwise.
+    """
+    if not graph:  # Handle the case where the graph is empty
+        return True
+
+    # Start from any node in the graph (grab an arbitrary key from the graph dictionary)
+    start_node = next(iter(graph))
+
+    # Use the reachable function to get all nodes that can be reached from the start node
+    reachable_nodes = reachable(graph, start_node)
+
+    # Check if the number of reachable nodes is equal to the number of nodes in the graph
+    return len(reachable_nodes) == len(graph)
 
 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
+  """
+  Returns the number of connected components in an undirected graph.
+  """
+  visited = set()
+  component_count = 0
+
+  for node in graph:
+      if node not in visited:
+          # Get all nodes reachable from the current node
+          reachable_nodes = reachable(graph, node)
+          # Add these nodes to the visited set
+          visited.update(reachable_nodes)
+          # Increment the component count for each set of connected nodes
+          component_count += 1
+
+  return component_count
 
